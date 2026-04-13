39c41a7ce693df8d903b19b6e32d25667110839b
remi <remid.fq.308@gmail.com>
2026-04-13 14:55:21 +0200

    création premières structures

--- entites.h
+++ entites.h
@@ -6,3 +6,35 @@
 #define HORCRUXES_HUNTER_ENTITES_H
 
 #endif //HORCRUXES_HUNTER_ENTITES_H
+
+typedef struct arme{
+    float x, y ;
+    float duree_restante ;
+    int active;
+    struct arme * suivant ;
+}t_arme;
+
+typedef struct joueur{
+    char pseudo[32] ;
+    int score ;
+    int niveau_actuel ;
+    float x, y ;
+    float vitesse ;
+    struct arme* arme_actuelle ;
+}t_joueur;
+
+typedef struct horcruxe{
+    float x, y ;
+    float vx,vy ;
+    int taille;
+    int lance_eclair;
+    struct arme * eclair;
+    struct horcruxe * suivant ;
+}t_hor;
+
+typedef struct aclair{
+    float x,y ;
+    float vitesse ;
+    struct eclair * suivant ;
+}t_eclair;
+
